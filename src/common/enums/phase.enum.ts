8dddbcb9e1a8944a5a3dd96f78126d6c8a4c6da8
Dennis Masso <dennismassomacias@gmail.com>
2024-04-21 19:23:17 -0500

    feat: add Document entity

---
+++ src/common/enums/phase.enum.ts
@@ -0,0 +1,8 @@
+/**
+ * Enum for the possible phases of a document.
+ */
+export enum Phase {
+    PHASE1 = 'Fase 1',
+    PHASE2 = 'Fase 2',
+    PHASE3 = 'Fase 3',
+}
--- src/documents/entities/document.entity.ts
+++ src/documents/entities/document.entity.ts
@@ -1,1 +1,24 @@
-export class Document {}
+import { Phase } from "src/common/enums/phase.enum";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Document {
+    @PrimaryGeneratedColumn()
+    readonly id: string;
+
+    @Column({ nullable: false })
+    readonly name: string;
+
+    @Column({ nullable: false, default: 0 })
+    readonly progress_percentage: number;
+
+    @Column({ type: 'enum', enum: Phase, nullable: false })
+    readonly phase: Phase;
+
+    @Column({ nullable: false })
+    readonly part: number;
+
+    // This column will contain all the information that the user writes.
+    @Column({ type: 'text', nullable: false })
+    readonly content: string;
+}
